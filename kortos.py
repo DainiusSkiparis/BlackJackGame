9bb37d1c02277c2d49e35746480b21747760cde1
Dainius Skiparis <dskiparis@gmail.com>
2023-08-14 22:47:23 +0300

    sumaisomos 6 kalades ir prideti pasirinkimai gavus kortas

--- kortos.py
+++ kortos.py
@@ -3,7 +3,7 @@ import random
 
 class Korta:
     def __init__(self, simbolis, reiksme):
-        self.simbol = simbolis
+        self.simbolis = simbolis
         self.reiksme = reiksme
         self.reiksmes_verte = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 10,
                                'Q': 10, 'K': 10, 'A': 11}
@@ -13,10 +13,17 @@ class KortuKalade:
     def __init__(self):
         self.simboliai = ['♠', '♥', '♦', '♣']
         self.reiksmes = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-        self.kalade = [Korta(simbolis, reiksme) for simbolis in self.simboliai for reiksme in self.reiksmes]
+        # testas self.simboliai = ['♠']
+        # testas self.reiksmes = ['J', 'Q']
+        # self.kalade = [Korta(simbolis, reiksme) for simbolis in self.simboliai for reiksme in self.reiksmes]
+
+        self.kaladziu_kiekis = 6  # Nurodome, kiek kaladžių norime sukurti
+        self.kalade = [Korta(simbolis, reiksme) for simbolis in self.simboliai for reiksme in self.reiksmes for _ in
+                       range(self.kaladziu_kiekis)]
+
         random.shuffle(self.kalade)
 
-    def traukti_korta(self):
+    def trauktikorta(self):
         if len(self.kalade) > 0:
             return self.kalade.pop()
         else:
@@ -25,3 +32,16 @@ class KortuKalade:
 
 # Sukuriamas kortų kaladės objektas
 kalade = KortuKalade()
+
+
+def zaidejokortos(kortos):
+    print("\nTavo kortos:", end=' ')
+    for korta in kortos:
+        print(f"{korta.reiksme}{korta.simbolis}", end=' ')
+    print()
+
+def dyleriokortos(kortos):
+    print("\nDylerio kortos:", end=' ')
+    for korta in kortos:
+        print(f"{korta.reiksme}{korta.simbolis}", end=' ')
+    print()
--- main.py
+++ main.py
@@ -1,20 +1,66 @@
-from kortos import kalade
+import random
 
-# Traukiame ir atspausdiname dvi kortas
-print("Traukiamos kortos:")
-dylerio_korta1 = kalade.traukti_korta()
-dylerio_korta2 = kalade.traukti_korta()
-zaidejo_korta1 = kalade.traukti_korta()
-zaidejo_korta2 = kalade.traukti_korta()
-zaidejo_korta3 = kalade.traukti_korta()
-zaidejo_korta4 = kalade.traukti_korta()
+from kortos import kalade, zaidejokortos, dyleriokortos
 
-# if korta1 and korta2:
-print(f"{dylerio_korta1.reiksme}{dylerio_korta1.simbol} {dylerio_korta2.reiksme}{dylerio_korta2.simbol}")
-suma1 = dylerio_korta1.reiksmes_verte[dylerio_korta1.reiksme] + dylerio_korta2.reiksmes_verte[dylerio_korta2.reiksme]
-print(f"Kortų suma: {suma1}")
+dylerio_suma = 0
+zaidejo_suma = 0
 
-print(f"{zaidejo_korta1.reiksme}{zaidejo_korta1.simbol} {zaidejo_korta2.reiksme}{zaidejo_korta2.simbol}")
-suma2 = zaidejo_korta1.reiksmes_verte[zaidejo_korta1.reiksme] + zaidejo_korta2.reiksmes_verte[zaidejo_korta2.reiksme]
-print(f"Kortų suma: {suma2}")
+dylerio_kortos = []
+zaidejo_kortos = []
 
+for _ in range(2):
+    # korta = kalade.trauktikorta()
+    dylerio_korta = random.choice(kalade.kalade)
+    zaidejo_korta = random.choice(kalade.kalade)
+    dylerio_suma += dylerio_korta.reiksmes_verte[dylerio_korta.reiksme]
+    zaidejo_suma += zaidejo_korta.reiksmes_verte[zaidejo_korta.reiksme]
+    dylerio_kortos.append(dylerio_korta)
+    zaidejo_kortos.append(zaidejo_korta)
+
+dyleriokortos(dylerio_kortos)
+zaidejokortos(zaidejo_kortos)
+
+print(f"Tavo suma: {zaidejo_suma}\n")
+
+while True:
+    print("+--------------------+")
+    print("| 1 - Imti kortą     |")
+    print("| 2 - Neimti kortos  |")
+    print("| 0 - Baigti žaidimą |")
+    print("+--------------------+")
+
+    pasirinkimas = input("Pasirinkite veiksmą: ")
+
+    if pasirinkimas == "1":
+        korta = random.choice(kalade.kalade)
+        # korta = kalade.trauktikorta()
+        zaidejo_suma += korta.reiksmes_verte[korta.reiksme]
+        zaidejo_kortos.append(korta)
+        zaidejokortos(zaidejo_kortos)
+        # print(f"Nauja korta: {korta.reiksme}{korta.simbolis}")
+        print(f"Tavo nauja suma: {zaidejo_suma}\n")
+
+    elif pasirinkimas == "2":
+        print("Pasirinkote neimti kortos.")
+        print(f"Tavo dabartinė suma: {zaidejo_suma}")
+
+    elif pasirinkimas == "0":
+        break
+
+    else:
+        print("Neteisingas pasirinkimas!")
+
+print("Žaidimo pabaiga")
+
+print("Dylerio kortos:")
+for korta in dylerio_kortos:
+    dylerio_suma += korta.reiksmes_verte[korta.reiksme]
+    print(f"{korta.reiksme}{korta.simbolis}")
+
+print(f"Dylerio suma: {dylerio_suma}\n")
+
+print("Tavo kortos:")
+for korta in zaidejo_kortos:
+    print(f"{korta.reiksme}{korta.simbolis}")
+
+print(f"Tavo suma: {zaidejo_suma}\n")
